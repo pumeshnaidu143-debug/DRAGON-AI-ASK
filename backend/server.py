ba7790c6cb3fa32ea5d4a7888ae30eaf1084ea5f
pumeshnaidu143-debug <p.umeshnaidu143@gmail.com>
2026-07-09 18:50:55 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -378,7 +378,7 @@ app.include_router(api)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=["https://dragon-ai-ask-5.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
